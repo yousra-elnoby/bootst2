d58394866cc2d09fe13bc78f2baf3495433be179
yousra-elnoby <138073589+yousra-elnoby@users.noreply.github.com>
2023-08-16 12:45:29 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var btn = document.getElementById("copybtn")
+var btn2 = document.getElementById("copybtn2")
+var btn3 = document.getElementById("copybtn3")
+var cop1 = document.getElementById("copy")
+var cop2 = document.getElementById("copy1")
+// window.onload = function(){alert("willkommen in Deutschland")}
+btn.onclick = function(){ alert("copied")
+}
+btn2.onclick = function(){ alert("copied")
+}
+btn3.onclick = function(){ alert("copied")
+}
+
+
